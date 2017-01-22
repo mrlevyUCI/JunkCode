6766b79fd793725a32c9722d292f54486a09b20c
Matthew Levy <mrlevy@uci.edu>
2017-01-21 19:43:58 -0800

    Initial commit

---
+++ Doctor App/ViewController.swift
@@ -0,0 +1,41 @@
+//
+//  ViewController.swift
+//  Doctor App
+//
+//  Created by Matthew Levy on 1/12/17.
+//  Copyright © 2017 Matthew Levy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    var pressed: Int = 0
+
+    
+    @IBOutlet weak var button1: UIButton!
+    
+    
+    @IBAction func buttonpress(_ sender: Any) {
+        if pressed % 2 == 0 {
+            button1.setTitle("selected", for: .normal)
+            pressed+=1
+        }
+        else {
+            button1.setTitle("unselected", for: .normal)
+            pressed+=1
+        }
+        
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        button1.setTitle("Matt", for: .normal)
+        
+    }
+
+    
+
+
+}
+
---
+++ Doctor App/storyboard1.swift
@@ -0,0 +1,12 @@
+//
+//  storyboard1.swift
+//  Doctor App
+//
+//  Created by Matthew Levy on 1/15/17.
+//  Copyright © 2017 Matthew Levy. All rights reserved.
+//
+import UIKit
+import Foundation
+class storyboard1: UIViewController {
+    
+}
